48fa0b248f63f6b897fc628e8dde2998e77f4caf
rklusa <ryan.klusacek@hotmail.ca>
2024-07-17 14:43:07 -0400

    Moved: Api calls to inside riverdatacomponent to make the component act more independently. Changed: button from set station id to add chart to make it easier for the user to add a chart

--- cascade.client/src/App.jsx
+++ cascade.client/src/App.jsx
@@ -4,55 +4,24 @@ import RiverDataComp from './RiverDataComponent';
 
 
 function App() {
-    const [data, setData] = useState();
-    const [stationName, setStationName] = useState();
-    const [loading, setLoading] = useState();
+
     const [stationId, setStationId] = useState();
     const [tempId, setTempId] = useState();
     const [charts, setCharts] = useState([]);
  
     //const stationId = "02ED027";
 
-    useEffect(() => {
-        if (stationId === null || stationId === "") {
-            return;
-        }
-        console.log("making api calls");
-        populateRiverData(stationId);
-        GetStationDetails(stationId);
-    }, [stationId]);
-
     return (
         <div>
-            <input value={tempId} onChange={e => setTempId(e.target.value)} />
-            <button onClick={HandleClick}> Set Station</button> <br />
+            <input value={stationId} onChange={e => setStationId(e.target.value)} />
+            <button onClick={AddChart}> Add Chart</button> <br />
             {charts}
         </div>
 
     );
 
-    function HandleClick() {
-        setStationId(tempId);
-        setCharts([...charts, <RiverDataComp _data={data} _stationName={stationName} _loading={loading} />])
-    }
-
-
-    async function populateRiverData(station) {
-
-        setLoading(true);
-        const response = await fetch(`GetRiverData?station=${station}`)
-        const data = await response.json();
-        setData(data);
-        setLoading(false);
-    }
-
-    async function GetStationDetails(station) {
-
-        const response = await fetch(`GetStationName?station=${station}`);
-        const stationName = await response.text();
-
-        setStationName(stationName);
-
+    function AddChart() {
+        setCharts([...charts, <RiverDataComp _stationId={stationId} />])
     }
 }
 
--- cascade.client/src/RiverDataComponent.jsx
+++ cascade.client/src/RiverDataComponent.jsx
@@ -1,13 +1,62 @@
 import Chart from './Chart';
 import LoadingSpinner from './Loading';
+import { useEffect, useState } from 'react';
 
-function RiverDataComp({_data, _stationName, _loading }) {
+function RiverDataComp({ _stationId, _stationName, _loading }) {
+    const [loading, setLoading] = useState();
+    const [data, setData] = useState();
+    const [stationName, setStationName] = useState();
+
+    useEffect(() => {
+        if (_stationId === null || _stationId === "") {
+            return;
+        }
+        console.log("making api calls");
+        populateRiverData(_stationId);
+        GetStationDetails(_stationId);
+    }, []);
+
     return (
         <div>
-            <text> {_stationName} </text>
-            {_loading ? <LoadingSpinner /> : <Chart _data={_data} />}
+            <text> {stationName} </text>
+            {loading ? <LoadingSpinner /> : <Chart _data={data} />}
         </div>
     );
+
+    async function populateRiverData(station) {
+
+        //setLoading(true);
+        //const response = await fetch(`GetRiverData?station=${station}`)
+        //const data = await response.json();
+        //setData(data);
+        //setLoading(false);
+
+        setLoading(true);
+
+        try {
+            const response = await fetch(`GetRiverData?station=${station}`)
+
+            if (!response.ok) {
+                throw new Error(response.statusText);
+            }
+            const data = await response.json();
+            setData(data);
+        } catch (err) {
+            console.log(err);
+        }
+
+        setLoading(false);
+
+    }
+
+    async function GetStationDetails(station) {
+
+        const response = await fetch(`GetStationName?station=${station}`);
+        const stationName = await response.text();
+
+        setStationName(stationName);
+
+    }
 }
 
 export default RiverDataComp;
